688737ff5c147d41643597534ea0b2324feaaffc
Adam Panzer <apanzer@zendesk.com>
2016-08-04 14:19:03 -0700

    more testing

--- spec/brakecheck_spec.rb
+++ spec/brakecheck_spec.rb
@@ -5,13 +5,16 @@ describe Brakecheck do
   let(:version_string) { "1.0.0" }
   let(:version) do
     version = double(:version)
-    expect(version).to receive(:version).and_return(version_string)
+    allow(version).to receive(:version).and_return(version_string)
     version
   end
 
   before do
     stub_request(:get, "https://rubygems.org/api/v1/versions/foo/latest.json").
       to_return(status: 200, body: %q{{"version": "1.0.0"}}, headers: {'Content-Type' => 'application/json'})
+    stub_request(:get, "https://rubygems.org/api/v1/versions/bar/latest.json").
+      to_return(status: 200, body: %q{{"version": "1.1.0"}}, headers: {'Content-Type' => 'application/json'})
+
     expect(Gem).to receive(:loaded_specs).and_return({'foo' => version})
   end
 
@@ -22,7 +25,18 @@ describe Brakecheck do
   end
 
   describe 'when the gem is not in the current bundle' do
-    let(:version_string) { '1.1.0' }
+    it 'fails' do
+      expect{ expect_latest('bar', loaded_specs('bar')) }.to raise_error(RSpec::Expectations::ExpectationNotMetError)
+    end
+
+    it 'says it is not in the bundle' do
+      expect(loaded_specs('bar')).to eq(:not_in_bundle)
+    end
+  end
+
+  describe 'when we have the wrong version' do
+    let(:version_string) { '0.0.9' }
+
     it 'fails' do
       expect{ expect_latest('foo', loaded_specs('foo')) }.to raise_error(RSpec::Expectations::ExpectationNotMetError)
     end
