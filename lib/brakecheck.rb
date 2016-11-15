36292bee979c90e872293638c479a16b4c539c19
Adam Panzer <apanzer@zendesk.com>
2016-11-15 10:43:30 -0800

    Test::Unit support

--- lib/brakecheck.rb
+++ lib/brakecheck.rb
@@ -10,6 +10,18 @@ module Brakecheck
       new.client.get("#{gem_name}/latest.json").body['version']
     end
 
+    def self.loaded_specs(gem_name)
+      gem_here = specs.detect do |specs|
+        specs.name == gem_name
+      end
+
+      gem_here.nil? ? :not_in_bundle : gem_here.version.to_s
+    end
+
+    def self.specs
+      @specs ||= Bundler::LockfileParser.new(Bundler.read_file(Bundler.default_lockfile)).specs
+    end
+
     def client
       @client = Faraday.new(:url => "https://rubygems.org/api/v1/versions") do |faraday|
         faraday.request       :url_encoded
@@ -19,12 +31,24 @@ module Brakecheck
     end
   end
 
+  module TestUnit
+    require 'test/unit/assertions'
+
+
+    def assert_latest_version(gem_name, message = nil)
+      message = build_message message, '<?> is not currently up to date.', gem_name
+      assert_block message do
+        assert_equal(Brakecheck::Core.latest(gem_name), Brakecheck::Core.loaded_specs(gem_name))
+      end
+    end
+  end
+
   module Rspec
     require 'rspec/expectations'
 
     RSpec::Matchers.define :be_the_latest_version do
       match do |gem_name|
-        spec_from_file = loaded_specs(gem_name)
+        spec_from_file = Brakecheck::Core.loaded_specs(gem_name)
         Brakecheck::Core.latest(gem_name) == spec_from_file
       end
 
@@ -35,18 +59,8 @@ module Brakecheck
           "expected #{gem_name} to be #{Brakecheck::Core.latest(gem_name)} but was actually #{loaded_specs(gem_name)}."
         end
       end
-
-      def loaded_specs(gem_name)
-        gem_here = specs.detect do |specs|
-          specs.name == gem_name
-        end
-
-        gem_here.nil? ? :not_in_bundle : gem_here.version.to_s
-      end
-
-      def specs
-        @specs ||= Bundler::LockfileParser.new(Bundler.read_file(Bundler.default_lockfile)).specs
-      end
     end
   end
 end
+
+Test::Unit::TestCase.prepend(Brakecheck::TestUnit)
